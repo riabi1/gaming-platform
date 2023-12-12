48690994ec16929f1c9768cdc7d205bd2d877e76
riabi1 <riabi.mar@gmail.com>
2023-12-12 23:55:23 +0100

    login/register

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component , OnInit} from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
 import { ModalService } from '../../services/modal.service';
 
 @Component({
@@ -7,17 +7,20 @@ import { ModalService } from '../../services/modal.service';
   styleUrl: './modal.component.css',
   // providers:[ModalService]
 })
-export class ModalComponent implements OnInit{
+export class ModalComponent implements OnInit {
+  @Input() modalID = '';
 
-  constructor(public modal: ModalService) {
-    
-  }
+  constructor(
+    public modal: ModalService,
+    private el: ElementRef) {
+
+    }
 
   ngOnInit(): void {
-
+    document.body.appendChild(this.el.nativeElement)
   }
 
-  closeModal(){
-    this.modal.toggleModal()
+  closeModal() {
+    this.modal.toggleModal(this.modalID);
   }
 }
